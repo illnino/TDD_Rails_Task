ad099e0f2a6413b21fa7beb5ae8574819b85d43e
illnino ho <illnino.giggs@gmail.com>
2012-10-25 22:05:21 +0800

    finished edit task

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -1,10 +1,11 @@
 require 'spec_helper'
 
 describe "Tasks" do
+	before do
+  		@task = Task.create :task => 'go to bed'
+	end	
   describe "GET /tasks" do
   	it "should display some tasks" do
-  		@task = Task.create :task => 'go to bed'
-
   		visit tasks_path
   		page.should have_content 'go to bed'
   	end
@@ -17,7 +18,35 @@ describe "Tasks" do
   		current_path.should == tasks_path
   		page.should have_content 'go to work'
 
-  		save_and_open_page
+  		# save_and_open_page
   	end 
+  end
+
+  describe "PUT /tasks" do
+  	it "should edit a task" do
+  		visit tasks_path
+  		click_link 'Edit'
+
+  		current_path.should == edit_task_path(@task)
+
+  		find_field('Task').value.should == 'go to bed'
+
+  		fill_in 'Task', :with => 'updated task'
+  		click_button 'Update Task'
+
+  		page.should have_content 'updated task'
+
+  	end
+
+    it "should not update an empty task" do
+        visit tasks_path
+        click_link 'Edit'
+
+        fill_in 'Task', :with => ''
+        click_button 'Update Task'
+
+        current_path.should == edit_task_path(@task)
+        page.should have_content 'There was an error'
+    end
   end
 end
