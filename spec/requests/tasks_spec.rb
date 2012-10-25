1f83b6ba43ae9961d4abdb69040c75998b9210f2
illnino ho <illnino.giggs@gmail.com>
2012-10-25 21:12:53 +0800

    finish task.index test

--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -3,10 +3,12 @@ require 'spec_helper'
 describe "Tasks" do
   describe "GET /tasks" do
   	it "should display some tasks" do
+  		@task = Task.create :task => 'go to bed'
+
   		visit tasks_path
   		page.should have_content 'go to bed'
 
-  		
+
   	end
   end
 end
