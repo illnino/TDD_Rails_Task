5e994eb996b96843df38acc521948d13834182d1
illnino ho <illnino.giggs@gmail.com>
2012-10-25 21:28:04 +0800

    finish create a new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,4 +2,9 @@ class TasksController < ApplicationController
   def index
   	@tasks = Task.all
   end
+
+  def create
+  	@task = Task.create params[:task]
+  	redirect_to :back
+  end
 end
--- spec/requests/tasks_spec.rb
+++ spec/requests/tasks_spec.rb
@@ -7,8 +7,17 @@ describe "Tasks" do
 
   		visit tasks_path
   		page.should have_content 'go to bed'
-
-
   	end
+
+  	it "create a new task" do
+  		visit  tasks_path
+  		fill_in 'Task', :with => 'go to work'
+  		click_button 'Create Task'
+
+  		current_path.should == tasks_path
+  		page.should have_content 'go to work'
+
+  		save_and_open_page
+  	end 
   end
 end
